5f8554ec4fc3957cb9c9e339bd382ff2dda8a86a
Patricio de la Guardia <padlgt01@gmail.com>
2018-10-18 16:06:05 -0400

    Switch key order

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const http = require('http');
 
 const port = process.env.PORT || 5000;
 
-const finance = ['Banker', 'Speculator', 'Spy', 'Farmer', 'Capitalist']
-const specialInterest = [
-	'Missionary', 'Foreign Consular', 'Protestor', 'Communist', 'Politician', 
-	'Priest', 'Peacekeeper', 'Lawyer', 'Customs Officer', 'Intellectual'
-]
-const communications = ['Newscaster','Reporter','Writer', 'Director', 'Producer']
-const force = ['Judge', 'Mercenary', 'General', 'Guerrilla', 'Crime Boss']
-
 function pickRandom(array) {
 	const randomIndex = Math.floor(Math.random() * array.length);
 	return array.splice(randomIndex, 1)[0];
 }
 
 function buildRoster() {
+	const finance = ['Banker', 'Speculator', 'Spy', 'Farmer', 'Capitalist']
+	const specialInterest = [
+		'Missionary', 'Foreign Consular', 'Protestor', 'Communist', 'Politician', 
+		'Priest', 'Peacekeeper', 'Lawyer', 'Customs Officer', 'Intellectual'
+	]
+	const communications = ['Newscaster','Reporter','Writer', 'Director', 'Producer']
+	const force = ['Judge', 'Mercenary', 'General', 'Guerrilla', 'Crime Boss']
+
 	let roster = {};
-	let categories = ['Finance', 'Special Interests', 'Communications', 'Force'];
+	roster['Special Interest'] = pickRandom(specialInterest);
+	let categories = ['Finance', 'Special Interests 2', 'Communications', 'Force'];
 	[finance, specialInterest, communications, force].forEach((el, i) => {
 		roster[categories[i]] = pickRandom(el);
 	});
-	roster['Special Interest 2'] = pickRandom(specialInterest);
 	return roster;
 }
 
