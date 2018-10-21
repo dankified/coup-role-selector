15118c4db6a1a3df28a3e2bdf9ba0713b4f15410
Patricio de la Guardia <padlgt01@gmail.com>
2018-10-21 09:01:08 -0400

    Move roster creation logic to frontend in order to avoid unnecessary requests to server

--- public/index.js
+++ public/index.js
@@ -1,33 +1,56 @@
+let roster =  {specialInterest: [
+  'Missionary', 'Foreign Consular', 'Protestor', 'Communist', 'Politician', 
+  'Priest', 'Peacekeeper', 'Lawyer', 'Customs Officer', 'Intellectual'
+], finance: ['Banker', 'Speculator', 'Spy', 'Farmer', 'Capitalist'], communications: ['Newscaster','Reporter','Writer', 'Director', 'Producer'], force: ['Judge', 'Mercenary', 'General', 'Guerrilla', 'Crime Boss']
+};
+
+let categories = {
+  specialInterest: "Special Interests",
+  specialInterest2: "Special Interests",
+  finance: "Finance",
+  communications: "Communications",
+  force: "Force"
+}
+
+function pickRandom(array) {
+	const randomIndex = Math.floor(Math.random() * array.length);
+	return array.splice(randomIndex, 1)[0];
+}
+
+function buildRoster(roster) {
+  let newRoster = JSON.parse(JSON.stringify(roster));
+  newRoster['Special Interest'] = pickRandom(newRoster.specialInterest);
+	newRoster['Special Interest 2'] = pickRandom(newRoster.specialInterest);
+	let categories = ['Finance', 'Communications', 'Force'];
+	['finance', 'communications', 'force'].forEach((el, i) => {
+		newRoster[categories[i]] = pickRandom(newRoster[el]);
+  });
+  return newRoster;
+}
+
+function createSection(root, roster, role) {
+  const div = document.createElement('DIV');
+  div.classList.add('role');
+  div.classList.add(categories[role].split(' ')[0]);
+  const p = document.createElement('P');
+  const text = document.createTextNode(`${categories[role]}: ${pickRandom(roster[role])}`);
+  p.appendChild(text);
+  div.appendChild(p);
+  root.appendChild(div);
+}
+
 function populateRoster(roster) {
   const root = document.getElementById('root');
   while(root.firstChild) root.removeChild(root.firstChild);
   for(let role in roster) {
-    const div = document.createElement('DIV');
-    div.classList.add('role');
-    div.classList.add(role.split(' ')[0]);
-    const p = document.createElement('P');
-    const text = document.createTextNode(`${role}: ${roster[role]}`);
-    p.appendChild(text);
-    div.appendChild(p);
-    root.appendChild(div);
+    createSection(root, roster, role);
+    if(categories[role] === 'Special Interests') createSection(root, roster, role)
   }
 }
 
-function fetchRoster() {
-  const request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-      const roster = JSON.parse(this.responseText);
-      populateRoster(roster);
-    }
-  };
-  request.open('GET', '/roster', true);
-  request.send();
-};
-
 (function() {
-  fetchRoster();
+  populateRoster(JSON.parse(JSON.stringify(roster)))
   document.getElementById('new-roster').addEventListener('click', () => {
-    fetchRoster();
+    populateRoster(JSON.parse(JSON.stringify(roster)));
   });
 }());
